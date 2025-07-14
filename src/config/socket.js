0ca46a43913e04bbc1831e7c3335c054f6ea67d2
ismail <ismailbary2@gmail.com>
2025-07-14 13:09:05 +0800

    edit env

--- src/config/socket.js
+++ src/config/socket.js
@@ -167,7 +167,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"],
+        origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://bantaeng-dessert.vercel.app"],
         credentials: true,
         transports: ['websocket', 'polling']
     },
